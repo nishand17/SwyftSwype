c3be093dd8f1a4e1f8f6b60f3df8b706005b7532
Nishan D'Souza <nishand@gmail.com>
2016-06-25 18:14:00 -0700

    Improvements

--- SwyftSwype/Level1Controller.swift
+++ SwyftSwype/Level1Controller.swift
@@ -11,16 +11,16 @@ import UIKit;
 
 class Level1Controller : UIViewController {
     
-    @IBOutlet weak var bae: UILabel!
+    @IBOutlet weak var output: UILabel!
     var colorNum = 0;
     var autoCC = false;
     var interval:Double = 2;
     var time = 0;
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.bae.text = "";
+        self.output.text = "";
         // Do any additional setup after loading the view, typically from a nib.
-        var swipeRight = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+        var swipeRight = UISwipeGestureRecognizer(target: self, action:"respondToSwipeGesture:")
         swipeRight.direction = UISwipeGestureRecognizerDirection.Right
         self.view.addGestureRecognizer(swipeRight)
         
@@ -60,64 +60,60 @@ class Level1Controller : UIViewController {
             case UISwipeGestureRecognizerDirection.Right:
                 print("Swiped right")
                 if(self.view.backgroundColor == UIColor.redColor()) {
-                    print("Yay we did it")
                     self.view.backgroundColor = UIColor.whiteColor()
-                    self.bae.text = "Correct!";
+                    self.output.text = "Correct!";
                     
                 }
                 else {
-                    print("NO")
+                    print("Error")
                     var endTimer = NSTimer.scheduledTimerWithTimeInterval(4, target:self, selector:"endGame", userInfo:nil,repeats:false)
-                    self.bae.text = ""
+                    self.output.text = ""
                     let levelVC = self.storyboard?.instantiateViewControllerWithIdentifier("LevelController") as! LevelController
                     self.navigationController?.pushViewController(levelVC, animated: true)
                 }
             case UISwipeGestureRecognizerDirection.Down:
                 print("Swiped down")
                 if(self.view.backgroundColor == UIColor.greenColor()) {
-                    print("Yay we did it")
                     self.view.backgroundColor = UIColor.whiteColor()
-                    self.bae.text = "Correct!";
+                    self.output.text = "Correct!";
                 }
                 else {
-                    print("NO")
+                    print("Error")
                     var endTimer = NSTimer.scheduledTimerWithTimeInterval(4, target:self, selector:"endGame", userInfo:nil,repeats:false)
-                    self.bae.text = ""
+                    self.output.text = ""
                     let levelVC = self.storyboard?.instantiateViewControllerWithIdentifier("LevelController") as! LevelController
                     self.navigationController?.pushViewController(levelVC, animated: true)
                 }
             case UISwipeGestureRecognizerDirection.Left:
                 print("Swiped left")
                 if(self.view.backgroundColor == UIColor.yellowColor()) {
-                    print("Yay we did it")
                     self.view.backgroundColor = UIColor.whiteColor()
-                    self.bae.text = "Correct!";
+                    self.output.text = "Correct!";
                     if(colorNum == 11) {
                         var winTimer = NSTimer.scheduledTimerWithTimeInterval(4, target:self, selector: "winGame", userInfo: nil, repeats: false)
-                        self.bae.text = ""
+                        self.output.text = ""
                         let levelVC = self.storyboard?.instantiateViewControllerWithIdentifier("LevelController") as! LevelController
                         self.navigationController?.pushViewController(levelVC, animated: true)
                     }
                 }
                 else {
-                    print("NO")
+                    print("Error")
                     var endTimer = NSTimer.scheduledTimerWithTimeInterval(4, target:self, selector:"endGame", userInfo:nil,repeats:false)
-                    self.bae.text = ""
+                    self.output.text = ""
                     let levelVC = self.storyboard?.instantiateViewControllerWithIdentifier("LevelController") as! LevelController
                     self.navigationController?.pushViewController(levelVC, animated: true)
                 }
             case UISwipeGestureRecognizerDirection.Up:
                 print("Swiped up")
                 if(self.view.backgroundColor == UIColor.blueColor()) {
-                    print("Yay we did it")
                     self.view.backgroundColor = UIColor.whiteColor()
-                    self.bae.text = "Correct!";
+                    self.output.text = "Correct!";
                     
                 }
                 else {
-                    print("NO")
+                    print("Error")
                     var endTimer = NSTimer.scheduledTimerWithTimeInterval(4, target:self, selector:"endGame", userInfo:nil,repeats:false)
-                    self.bae.text = ""
+                    self.output.text = ""
                     let levelVC = self.storyboard?.instantiateViewControllerWithIdentifier("LevelController") as! LevelController
                     self.navigationController?.pushViewController(levelVC, animated: true)
                 }
@@ -134,33 +130,33 @@ class Level1Controller : UIViewController {
     func changeColor() {
         switch(colorNum) {
         case 0: self.view.backgroundColor = UIColor.blueColor()
-            self.bae.text = "";
+            self.output.text = "";
         case 1: self.view.backgroundColor = UIColor.redColor()
-            self.bae.text = "";
+            self.output.text = "";
         case 2: self.view.backgroundColor = UIColor.greenColor()
-            self.bae.text = "";
+            self.output.text = "";
         case 3: self.view.backgroundColor = UIColor.yellowColor()
-            self.bae.text = "";
+            self.output.text = "";
         case 4: self.view.backgroundColor = UIColor.redColor()
-            self.bae.text = "";
+            self.output.text = "";
         case 5: self.view.backgroundColor = UIColor.blueColor()
-            self.bae.text = "";
+            self.output.text = "";
         case 6: self.view.backgroundColor = UIColor.redColor()
-            self.bae.text = "";
+            self.output.text = "";
         case 7: self.view.backgroundColor = UIColor.greenColor()
-            self.bae.text = "";
+            self.output.text = "";
         case 8: self.view.backgroundColor = UIColor.blueColor()
-            self.bae.text = "";
+            self.output.text = "";
         case 9: self.view.backgroundColor = UIColor.yellowColor()
-            self.bae.text = "";
+            self.output.text = "";
         case 10: self.view.backgroundColor = UIColor.blueColor()
-            self.bae.text = "";
+            self.output.text = "";
         case 11: self.view.backgroundColor = UIColor.yellowColor()
-            self.bae.text = "";
+            self.output.text = "";
             
         default: break;
         }
-        colorNum++
+        colorNum += 1
     }
     
     func runLevel() {
@@ -168,9 +164,9 @@ class Level1Controller : UIViewController {
         viewDidLoad()
     }
     func endGame() {
-        self.bae.text = "You failed! Try again"
+        self.output.text = "You failed! Try again"
     }
     func winGame() {
-        self.bae.text = "You win!"
+        self.output.text = "You win!"
     }
 }
